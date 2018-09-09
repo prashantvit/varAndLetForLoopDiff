e7b5ea8f02af02e6c44b19ef8d42864148548300
Prashant Choudhary <prashant.srs@gmail.com>
2018-09-09 23:55:31 +0530

    self executing function closure concept

--- src/index.js
+++ src/index.js
@@ -10,4 +10,12 @@ for (let i = 0; i < 10; i++) {
   }, i * 1000);
 }
 
+for (var i = 1; i <= 3; i++) {
+  (index => {
+    setTimeout(function() {
+      console.log(index);
+    }, i * 1000);
+  })(i);
+}
+
 // Standard interview question for diff between var and let
